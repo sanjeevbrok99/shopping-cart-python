4f86698f725dac80498078dd3b3299fab2aaaee9
Sanjeev Mishra <mishragsanjeev1999@gmail.com>
2023-02-22 21:59:21 +0530

    mdoel designed implemantation done for cart item

--- shopping_cart/api_app/models.py
+++ shopping_cart/api_app/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class CartItem(models.Model):
+    product_name = models.CharField(max_length=200)
+    product_price = models.FloatField()
+    product_quantity = models.PositiveIntegerField()
