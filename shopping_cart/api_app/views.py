b94add641e0d8aac43ac0182840fae2f83530e22
Sanjeev Mishra <mishragsanjeev1999@gmail.com>
2023-02-22 23:14:16 +0530

    refactoring done

--- shopping_cart/api_app/views.py
+++ shopping_cart/api_app/views.py
@@ -7,9 +7,9 @@ from .models import CartItem
 
 @method_decorator(csrf_exempt, name='dispatch')
 class ShoppingCart(View):
-    def post(self, request):
 
-        data = json.loads(request.body.decode("utf-8"))
+    def post(self, request):
+        data = json.loads(request.body.decode("utf-8"))  
         p_name = data.get('product_name')
         p_price = data.get('product_price')
         p_quantity = data.get('product_quantity')
@@ -23,21 +23,14 @@ class ShoppingCart(View):
         cart_item = CartItem.objects.create(**product_data)
 
         data = {
-            "message": f"New item added to Cart with id: {cart_item.id}"
-        }
-        return JsonResponse(data, status=201)
-
-@method_decorator(csrf_exempt, name='dispatch')
-class ShoppingCart(View):
-
-    def post(self, request):
-        ...
+            "message": f"New item added to Cart with id: {cart_item.id}"}
+        return JsonResponse(data, status=200)
 
     def get(self, request):
-        items_count = CartItem.objects.count()
-        items = CartItem.objects.all()
+        items_count = CartItem.objects.count() 
+        items = CartItem.objects.all()  
 
-        items_data = []
+        items_data = []  
         for item in items:
             items_data.append({
                 'product_name': item.product_name,
@@ -49,8 +42,8 @@ class ShoppingCart(View):
             'items': items_data,
             'count': items_count,
         }
-        print("Hello")
         return JsonResponse(data)
+           
 
 
 @method_decorator(csrf_exempt, name='dispatch')
@@ -61,25 +54,19 @@ class ShoppingCartUpdate(View):
         item = CartItem.objects.get(id=item_id)
         item.product_quantity = data['product_quantity']
         item.save()
-
+        
         data = {
             'message': f'Item {item_id} has been updated'
         }
 
         return JsonResponse(data)
 
-@method_decorator(csrf_exempt, name='dispatch')
-class ShoppingCartUpdate(View):
-
-    def patch(self, request, item_id):
-        ...
-
     def delete(self, request, item_id):
         item = CartItem.objects.get(id=item_id)
         item.delete()
-
+        
         data = {
             'message': f'Item {item_id} has been deleted'
         }
 
-        return JsonResponse(data)               
+        return JsonResponse(data)
